667494039ead0dfa4c8ef3c12421ce82b702ddce
ZPiyapanZ <66814546+ZPiyapanZ@users.noreply.github.com>
2022-06-20 15:28:45 +0700

    Delete swagger in main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-    .setTitle('Bookstore API')
-    .setVersion('1.0.0')
-    .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
-      'access-token',
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
